d0e9fd6e2c1f8f1ff1b6cbb30564284261146c1b
TobyG74 <tobisaputra11@gmail.com>
2023-11-06 00:50:59 +0700

    feat: update types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,1 @@
-export * from "./utils/dl_tiktokapi"
-export * from "./utils/dl_ssstik"
-export * from "./utils/dl_musicaldown"
-export * from "./utils/tiktok_stalker"
+export * from "./utils/switch"
